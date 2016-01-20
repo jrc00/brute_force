d9ee3288f652607a7dbc85ad6806225b9c4966d6
Jeff Carter <jcarter00@gmail.com>
2016-01-20 10:35:56 -0500

    fixed single table ingeritance issue, for real this time.

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -69,6 +69,6 @@ class ResourcesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def resource_params
-      params.require(:resource).permit(:title, :language, :type, :link, :completed)
+      params.require(:resource).permit(:title, :language, :category, :link, :completed)
     end
 end
