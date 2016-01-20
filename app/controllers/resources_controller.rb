d72053bd0d62846e4a7b117056c571fc3a076ae9
Jeff Carter <jcarter00@gmail.com>
2016-01-19 21:57:40 -0500

    added flash messages

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,6 +1,6 @@
 class ResourcesController < ApplicationController
   before_action :set_resource, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!
   # GET /resources
   # GET /resources.json
   def index
